1ca30b75095fa4bacad9a5fbb6e51dacf1a66db2
Lucas Easley <lucaseasley11@gmail.com>
2020-05-21 00:00:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @name = param[":name"]
+    @coach = param[":coach"]
+    @pg = param[":pg"]
+    @sg = param[":sg"]
+    @sf = param[":sf"]
+    @pf = param[":pf"]
+    @c = param[":c"]
     erb :team
   end
 
